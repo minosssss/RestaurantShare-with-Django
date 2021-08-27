866e71b1d596742470b1cfe172675e9a1b98d4e9
mh.lee <mh.lee239@gmail.com>
2021-08-27 20:02:45 +0900

    modified vews,urls,index.html

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 # Create your views here.
 
 
 def index(request):
     # return HttpResponse("index")
-    return render(request, 'shareRes/index.html')
+    categories = Category.objects.all()
+    content = {'categories': categories}
+    return render(request, 'shareRes/index.html', content)
 
 
 def restaurantDetail(request):
@@ -17,4 +21,16 @@ def restaurantCreate(request):
 
 
 def categoryCreate(request):
-    return render(request, 'shareRes/categoryCreate.html')
+    categories = Category.objects.all()
+    content = {'categories': categories}
+    return render(request, 'shareRes/categoryCreate.html', content)
+    # return render(request, 'shareRes/categoryCreate.html')
+
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name=category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+
+    # return HttpResponse("category create 기능구현 필요")
