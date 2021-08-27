b6ac8c4117791f1d2b56cc392075d0ce7832f3d0
mh.lee <mh.lee239@gmail.com>
2021-08-27 13:40:51 +0900

    setting urls,views

--- RestaurantShare/shareRes/models.py
+++ RestaurantShare/shareRes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+
+class Restaurant(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_DEFAULT, default=3)
+    restaurant_name = models.CharField(max_length=100)  # 맛집 이름
+    restaurant_link = models.CharField(max_length=500)  # 맛집 URL
+    restaurant_content = models.TextField()  # 맛집설명
+    restaurant_keyword = models.CharField(max_length=50)  # 키워드
